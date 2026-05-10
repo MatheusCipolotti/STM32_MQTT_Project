852c0b5ea9996f33bcf303f5f360970ac08add68
MDC <dantas_net@hotmail.com>
2026-05-10 20:23:12 -0300

    V0.1

--- Software/MQTT_Project/Debug/Ethernet_W5500/wizchip_port.d
+++ Software/MQTT_Project/Debug/Ethernet_W5500/wizchip_port.d
@@ -6,7 +6,10 @@ Ethernet_W5500/wizchip_port.o: ../Ethernet_W5500/wizchip_port.c \
  /media/matheus-cipolotti/MDC/SENAI/TCC/Software/MQTT_Project/CMSIS/Include/cmsis_gcc.h \
  /media/matheus-cipolotti/MDC/SENAI/TCC/Software/MQTT_Project/CMSIS/Include/mpu_armv7.h \
  /media/matheus-cipolotti/MDC/SENAI/TCC/Software/MQTT_Project/CMSIS/Device/system_stm32g0xx.h \
- ../Ethernet_W5500/wizchip_port.h ../Inc/SPI.h
+ ../Inc/SPI.h ../Ethernet_W5500/wizchip_conf.h \
+ ../Ethernet_W5500/W5500/w5500.h \
+ /media/matheus-cipolotti/MDC/SENAI/TCC/Software/MQTT_Project/Ethernet_W5500/wizchip_conf.h \
+ ../Ethernet_W5500/wizchip_port.h ../Ethernet_W5500/socket.h
 /media/matheus-cipolotti/MDC/SENAI/TCC/Software/MQTT_Project/CMSIS/Device/stm32g0b0xx.h:
 /media/matheus-cipolotti/MDC/SENAI/TCC/Software/MQTT_Project/CMSIS/Include/core_cm0plus.h:
 /media/matheus-cipolotti/MDC/SENAI/TCC/Software/MQTT_Project/CMSIS/Include/cmsis_version.h:
@@ -14,5 +17,9 @@ Ethernet_W5500/wizchip_port.o: ../Ethernet_W5500/wizchip_port.c \
 /media/matheus-cipolotti/MDC/SENAI/TCC/Software/MQTT_Project/CMSIS/Include/cmsis_gcc.h:
 /media/matheus-cipolotti/MDC/SENAI/TCC/Software/MQTT_Project/CMSIS/Include/mpu_armv7.h:
 /media/matheus-cipolotti/MDC/SENAI/TCC/Software/MQTT_Project/CMSIS/Device/system_stm32g0xx.h:
-../Ethernet_W5500/wizchip_port.h:
 ../Inc/SPI.h:
+../Ethernet_W5500/wizchip_conf.h:
+../Ethernet_W5500/W5500/w5500.h:
+/media/matheus-cipolotti/MDC/SENAI/TCC/Software/MQTT_Project/Ethernet_W5500/wizchip_conf.h:
+../Ethernet_W5500/wizchip_port.h:
+../Ethernet_W5500/socket.h:
